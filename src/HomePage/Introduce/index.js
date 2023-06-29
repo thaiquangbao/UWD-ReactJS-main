e86981cf74da6737659d76ae85d9bbfce080d8aa
thaiquangbao <97949012+thaiquangbao@users.noreply.github.com>
2023-06-29 13:33:55 +0700

    upload

--- src/HomePage/Introduce/index.js
+++ src/HomePage/Introduce/index.js
@@ -11,13 +11,13 @@ function Introduce() {
                 <div className='connect-team col-lg-8'>
                     <div className='image-user'>
                         <div className='item'>
-                            <img src='https://scontent.fsgn4-1.fna.fbcdn.net/v/t39.30808-6/334198654_747462426944725_8795385163218375556_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=NjZd4ya8xk4AX_B06Le&_nc_ht=scontent.fsgn4-1.fna&oh=00_AfBX72RdG0PI7xPeqq3IkE_FIVvNZVx_wn_I0-XgPFdI4w&oe=6494D014'  height='100%' />
+                            <img src='https://scontent.fsgn2-9.fna.fbcdn.net/v/t39.30808-6/331049606_776473217379002_4331964272007813158_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=IBqswXn7XSsAX9BSIap&_nc_ht=scontent.fsgn2-9.fna&oh=00_AfDgD0JOK3BFlLWlOD5XpOAtmnyOaO1_WCR_Ci74Tl7CUA&oe=64A16F3D'  height='100%' />
                         </div>
                         <div className='item'>
-                            <img src='https://scontent.fsgn4-1.fna.fbcdn.net/v/t39.30808-6/334198654_747462426944725_8795385163218375556_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=NjZd4ya8xk4AX_B06Le&_nc_ht=scontent.fsgn4-1.fna&oh=00_AfBX72RdG0PI7xPeqq3IkE_FIVvNZVx_wn_I0-XgPFdI4w&oe=6494D014'  height='100%' />
+                            <img src='https://scontent.fsgn2-9.fna.fbcdn.net/v/t39.30808-6/331049606_776473217379002_4331964272007813158_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=IBqswXn7XSsAX9BSIap&_nc_ht=scontent.fsgn2-9.fna&oh=00_AfDgD0JOK3BFlLWlOD5XpOAtmnyOaO1_WCR_Ci74Tl7CUA&oe=64A16F3D'  height='100%' />
                         </div>
                         <div className='item'>
-                            <img src='https://scontent.fsgn4-1.fna.fbcdn.net/v/t39.30808-6/334198654_747462426944725_8795385163218375556_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=NjZd4ya8xk4AX_B06Le&_nc_ht=scontent.fsgn4-1.fna&oh=00_AfBX72RdG0PI7xPeqq3IkE_FIVvNZVx_wn_I0-XgPFdI4w&oe=6494D014'  height='100%' />
+                            <img src='https://scontent.fsgn2-9.fna.fbcdn.net/v/t39.30808-6/331049606_776473217379002_4331964272007813158_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=IBqswXn7XSsAX9BSIap&_nc_ht=scontent.fsgn2-9.fna&oh=00_AfDgD0JOK3BFlLWlOD5XpOAtmnyOaO1_WCR_Ci74Tl7CUA&oe=64A16F3D'  height='100%' />
                         </div>
                     </div>
                     <div className='message'>
